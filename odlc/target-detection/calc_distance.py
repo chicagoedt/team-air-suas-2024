cee842af598bece40960457c768c9f18fd26f339
Manh Phan <dphan8@uic.edu>
2024-04-03 16:45:23 -0500

    document calc_distance.py

--- odlc/target-detection/calc_distance.py
+++ odlc/target-detection/calc_distance.py
@@ -1,4 +1,15 @@
 def get_target_location(target_x, target_y, image_width, image_height, latitude, longitude, width_feet, height_feet):
+    """
+        Translate the coordinate of target (px, px) to coordinate in real life (longtitude, latitude)
+        Input:
+            target_x, target_y: coordinate of target in image (px, px)
+            image_width, image_height: the size of image containing the target (px, px)
+            latitude, longtitude: the real coordinate of the drone capturing the image (degree, degree)
+            width_feet, height_feet: the size of the area captured in the image (ft)
+
+        Return:
+            latitude, longitude of the target (degree, degree)
+    """
     # Convert feet to degrees
     latitude_per_foot = 0.00000274
     longitude_per_foot = 0.00000429
@@ -13,6 +24,7 @@ def get_target_location(target_x, target_y, image_width, image_height, latitude,
 
     return (target_latitude, target_longitude)
 
-target_latitude, target_longitude = get_target_location(100, 200, 3040, 4032, 37.4419, -122.1430, 30, 40)
-print(f"Target latitude: {target_latitude}")
-print(f"Target longitude: {target_longitude}")
+if __name__ == "__main__":
+    target_latitude, target_longitude = get_target_location(100, 200, 3040, 4032, 37.4419, -122.1430, 30, 40)
+    print(f"Target latitude: {target_latitude}")
+    print(f"Target longitude: {target_longitude}")
